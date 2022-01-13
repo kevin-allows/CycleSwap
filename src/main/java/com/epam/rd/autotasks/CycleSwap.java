ad21eb9501e9711085bd330e8206503d1a72f3b6
Ilya Mankouski <ilya.mankouski@gmail.com>
2022-01-14 00:28:19 +0100

    CycleSwap - not even a try

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -1,9 +1,23 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
+
 class CycleSwap {
     static void cycleSwap(int[] array) {
-
+        int[] newArray = new int[array.length];
+        int last = array[array.length-1];
+        for (int i=0; i<array.length-1; i++){
+            newArray[i+1]=array[i];
+        }
+        newArray[0] = last;
+        array = Arrays.copyOf(newArray, array.length);
     }
+
+    /*
+    if (i == array.length-1){
+                array[0] = array[array.length-1];
+            }
+     */
 
     static void cycleSwap(int[] array, int shift) {
 
