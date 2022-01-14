ce01f17a42f6ab3950aae71b1aa74ca0ee63ee40
Ilya Mankouski <ilya.mankouski@gmail.com>
2022-01-14 01:13:32 +0100

    CycleSwap - Try1

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -5,21 +5,33 @@ import java.util.Arrays;
 class CycleSwap {
     static void cycleSwap(int[] array) {
         int[] newArray = new int[array.length];
-        int last = array[array.length-1];
-        for (int i=0; i<array.length-1; i++){
-            newArray[i+1]=array[i];
+        if (array.length>0){
+            int last = array[array.length-1];
+            for (int i=0; i<array.length-1; i++){
+                newArray[i+1]=array[i];
+            }
+            newArray[0] = last;
+            for (int i=0; i<array.length; i++){
+                array[i] = newArray[i];
+            }
+        } else {
+            return;
         }
-        newArray[0] = last;
-        array = Arrays.copyOf(newArray, array.length);
     }
 
-    /*
-    if (i == array.length-1){
-                array[0] = array[array.length-1];
-            }
-     */
-
     static void cycleSwap(int[] array, int shift) {
+        int[] newArray = new int[array.length];
+        for (int i=0; i<array.length; i++){
+            if ((i+shift)>(array.length-1)) {
+                newArray[(i+shift)-array.length] = array[i];
+            }
+            else {
+                newArray[i+shift] = array[i];
+            }
+        }
+        for (int i=0; i<array.length; i++) {
+            array[i] = newArray[i];
+        }
 
     }
 }
